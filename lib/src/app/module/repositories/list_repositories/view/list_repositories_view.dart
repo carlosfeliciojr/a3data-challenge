b244d1faab32fea958001ad2f7d14093df4525db
Carlos Felício <felicio.contato@gmail.com>
2023-03-20 18:21:46 -0300

    :building_construction: Adding base files for module list repositories

---
+++ lib/src/app/module/repositories/list_repositories/view/list_repositories_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListRepositoriesView extends StatelessWidget {
+  const ListRepositoriesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
