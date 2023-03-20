62020e8933db03856f2686f482eeeea29c70d97e
Carlos Felício <felicio.contato@gmail.com>
2023-03-20 16:11:28 -0300

    :construction: Implementing dependencies injection with get_it

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import './src/app/config/dependencies_injection.dart';
+
 void main() {
+  DependenciesInjection.registerDependencies();
   runApp(const MainApp());
 }
 
