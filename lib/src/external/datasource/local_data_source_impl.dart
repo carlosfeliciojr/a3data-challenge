ffb6aa7ba77ae649325f9844710564c710099b10
Carlos Felício <felicio.contato@gmail.com>
2023-03-20 18:18:53 -0300

    :building_construction: Adding base files for external

---
+++ lib/src/external/datasource/local_data_source_impl.dart
@@ -0,0 +1,3 @@
+import 'package:a3data_challenge/src/infra/data_source/local_data_source.dart';
+
+class LocalDataSourceImpl implements LocalDataSource {}
---
+++ lib/src/external/datasource/rest_api_data_source_impl.dart
@@ -0,0 +1,3 @@
+import 'package:a3data_challenge/src/infra/data_source/rest_api_data_source.dart';
+
+class RestApiDataSourceImpl implements RestApiDataSource {}
