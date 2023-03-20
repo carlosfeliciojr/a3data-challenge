96183647209d42465d2db57e4c2035a61101dc49
Carlos Felício <felicio.contato@gmail.com>
2023-03-20 18:21:20 -0300

    :building_construction: Adding base files for module favorites

---
+++ lib/src/app/module/repositories/favorites_repositories/view/favorite_repositories_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FavoritesRepositoriesView extends StatelessWidget {
+  const FavoritesRepositoriesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
