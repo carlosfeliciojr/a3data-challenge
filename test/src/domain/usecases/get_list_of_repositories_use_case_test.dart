340fbd62342798df6085e280fea5cd7c48d95701
Carlos Felício <felicio.contato@gmail.com>
2023-03-21 01:17:23 -0300

    :white_check_mark: Implementing GetListOfRepositoriesUseCase test

--- test/src/domain/usecases/get_list_of_repositories_use_case_test.dart
+++ test/src/domain/usecases/get_list_of_repositories_use_case_test.dart
@@ -1,40 +1,33 @@
 import 'package:a3data_challenge/src/domain/entities/repository_entity.dart';
 import 'package:a3data_challenge/src/domain/enums/code_language_enum.dart';
+import 'package:a3data_challenge/src/domain/params/get_list_of_repositories_params.dart';
 import 'package:a3data_challenge/src/domain/repositories/repository_repository.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
-
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:a3data_challenge/src/domain/usecases/get_list_of_repositories_use_case.dart';
 
-class MockRepositoryRepository extends Mock implements RepositoryRepository {}
+import 'get_list_of_repositories_use_case_test.mocks.dart';
 
+@GenerateNiceMocks([MockSpec<RepositoryRepository>()])
 void main() {
-  group('GetListOfRepositoriesUseCase', () {
-    setUpAll(() {
-      registerFallbackValue(CodeLanguageEnum.dart);
-    });
+  final RepositoryRepository repository = MockRepositoryRepository();
+  final GetListOfRepositoriesUseCase getListOfRepositoriesUseCase =
+      GetListOfRepositoriesUseCase(repository: repository);
 
-    late RepositoryRepository repository;
-    late GetListOfRepositoriesUseCase getListOfRepositoriesUseCase;
-
-    setUp(() {
-      repository = MockRepositoryRepository();
-      getListOfRepositoriesUseCase = GetListOfRepositoriesUseCase(
-        repository: repository,
+  group(
+    'GetListOfRepositoriesUseCase',
+    () {
+      final params = GetListOfRepositoriesParams(
+        language: CodeLanguageEnum.dart,
+        page: 1,
+        amountPerPage: 1,
       );
-    });
 
-    test(
-      'getListOfRepositoriesUseCase',
-      () {
-        when(
-          () async => repository.getListOfRepositories(
-            language: any(named: 'language'),
-            page: any(named: 'page'),
-            amountPerPage: any(named: 'amountPerPage'),
-          ),
-        ).thenAnswer(
-          (_) async => [
+      test(
+        'sucess - with data',
+        () async {
+          final expectedAnswer = [
             RepositoryEntity(
               name: "flutter",
               description:
@@ -43,33 +36,65 @@ void main() {
               language: CodeLanguageEnum.dart,
               watchers: 151346,
             ),
-          ],
-        );
+          ];
 
-        expect(
-          getListOfRepositoriesUseCase.call(
-            language: CodeLanguageEnum.dart,
-            page: 1,
-            amountPerPage: 1,
-          ),
-          equals([
-            RepositoryEntity(
-              name: "flutter",
-              description:
-                  "Flutter makes it easy and fast to build beautiful apps for mobile and beyond",
-              creationDate: DateTime(2015, 3, 6, 22, 54, 58),
-              language: CodeLanguageEnum.dart,
-              watchers: 151346,
-            ),
-          ]),
-        );
+          when(
+            repository.getListOfRepositories(params: params),
+          ).thenAnswer(
+            (_) async => expectedAnswer,
+          );
 
-        verify(() => getListOfRepositoriesUseCase.call(
-              language: any(),
-              page: 1,
-              amountPerPage: 1,
-            )).called(1);
-      },
-    );
-  });
+          final result = await getListOfRepositoriesUseCase.call(
+            params: params,
+          );
+
+          expect(result, isNotNull);
+          expect(result, isA<List<RepositoryEntity>>());
+          expect(result, equals(expectedAnswer));
+        },
+      );
+
+      test(
+        'sucess - empty data',
+        () async {
+          final expectedAnswer = <RepositoryEntity>[];
+
+          when(
+            repository.getListOfRepositories(params: params),
+          ).thenAnswer(
+            (_) async => expectedAnswer,
+          );
+
+          final result = await getListOfRepositoriesUseCase.call(
+            params: params,
+          );
+
+          expect(result, isNotNull);
+          expect(result, isA<List<RepositoryEntity>>());
+          expect(result, equals([]));
+        },
+      );
+
+      test(
+        'error',
+        () async {
+          final expectedAnswer = <RepositoryEntity>[];
+
+          when(
+            repository.getListOfRepositories(params: params),
+          ).thenAnswer(
+            (_) async => expectedAnswer,
+          );
+
+          final result = await getListOfRepositoriesUseCase.call(
+            params: params,
+          );
+
+          expect(result, isNotNull);
+          expect(result, isA<List<RepositoryEntity>>());
+          expect(result, equals([]));
+        },
+      );
+    },
+  );
 }
