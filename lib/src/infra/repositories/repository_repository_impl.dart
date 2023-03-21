306c52a94b0bd884f83675db30fdcf6133789e2d
Carlos Felício <felicio.contato@gmail.com>
2023-03-21 14:39:46 -0300

    :sparkles: Implementing RepositoryRepositoryImpl

--- lib/src/infra/repositories/repository_repository_impl.dart
+++ lib/src/infra/repositories/repository_repository_impl.dart
@@ -1,20 +1,44 @@
 import 'package:a3data_challenge/src/domain/repositories/repository_repository.dart';
-import 'package:a3data_challenge/src/infra/data_source/local_database.dart';
+import 'package:a3data_challenge/src/infra/data_source/database.dart';
 
 class RepositoryRepositoryImpl implements RepositoryRepository {
-  final LocalDatabase database;
+  final Database database;
 
   RepositoryRepositoryImpl({required this.database});
 
   @override
-  Future<void> getFavoritesRepositories() {
-    // TODO: implement getFavoritesRepositories
-    throw UnimplementedError();
+  Future<List<Map<String, dynamic>>> getListFavoritesFromDatabase() async {
+    final favoritesDatabase =
+        await database.getItem(key: 'favorites') ?? {"listFavorites": []};
+    final listFavorites =
+        List<Map<String, dynamic>>.from(favoritesDatabase["listFavorites"]);
+    return listFavorites;
   }
 
   @override
-  Future<void> saveFavoriteRepository() {
-    // TODO: implement saveFavoriteRepository
-    throw UnimplementedError();
+  Future<Map<String, dynamic>> updateRepositoryInDatabase({
+    required Map<String, dynamic> favorite,
+  }) async {
+    final listFavorites = await getListFavoritesFromDatabase();
+    final favoriteIndex = listFavorites.indexWhere(
+      (repository) => repository["id"] == favorite["id"],
+    );
+    listFavorites[favoriteIndex] = favorite;
+
+    await saveRepositoryInDatabase(newFavorite: listFavorites[favoriteIndex]);
+
+    return favorite;
+  }
+
+  @override
+  Future<void> saveRepositoryInDatabase({
+    required Map<String, dynamic> newFavorite,
+  }) async {
+    final listFavorites = await getListFavoritesFromDatabase();
+    listFavorites.add(newFavorite);
+    await database.setItem(
+      key: "favorites",
+      data: {"listFavorites": listFavorites},
+    );
   }
 }
