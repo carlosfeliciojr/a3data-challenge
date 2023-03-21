0de7d4c1c6c823898356c7b8a2553adb4a9284e2
Carlos Felício <felicio.contato@gmail.com>
2023-03-21 02:33:51 -0300

    :recycle: Adding factory to CodeLanguageEnum

--- lib/src/domain/enums/code_language_enum.dart
+++ lib/src/domain/enums/code_language_enum.dart
@@ -4,4 +4,8 @@ enum CodeLanguageEnum {
   final String text;
 
   const CodeLanguageEnum({required this.text});
+
+  factory CodeLanguageEnum.fromText({required String text}) {
+    return values.firstWhere((code) => code.text == text);
+  }
 }
