191685cd124fd91d4b7c38a45e89f81c1d20e2f3
Carlos Felício <felicio.contato@gmail.com>
2023-03-22 13:17:05 -0300

    :construction: Added mock for SearchUserWidget

---
+++ lib/src/app/module/repositories/list_repositories/widgets/search_user_widget.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SearchUserWidget extends StatefulWidget {
+  const SearchUserWidget({
+    super.key,
+    required this.onPressed,
+  });
+
+  final void Function()? onPressed;
+
+  @override
+  State<SearchUserWidget> createState() => _SearchUserWidgetState();
+}
+
+class _SearchUserWidgetState extends State<SearchUserWidget> {
+  late TextEditingController _textController;
+
+  @override
+  void initState() {
+    super.initState();
+    _textController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(
+          width: 28,
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 192 + 32),
+          child: CupertinoTextField(
+            controller: _textController,
+            enableSuggestions: true,
+            decoration: const BoxDecoration(
+              color: Color(0xff161B21),
+              borderRadius: BorderRadius.all(
+                Radius.circular(16),
+              ),
+            ),
+            style: const TextStyle(color: Color(0xffC9D1D9)),
+            maxLength: 39,
+            maxLengthEnforcement: MaxLengthEnforcement.enforced,
+            maxLines: 1,
+            placeholder: "Input GitHub user name...",
+            placeholderStyle: const TextStyle(color: Color(0xffC9D1D9)),
+          ),
+        ),
+        const SizedBox(width: 4),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.search, color: Color(0xffC9D1D9)),
+          constraints: const BoxConstraints(maxWidth: 24, maxHeight: 24),
+          padding: const EdgeInsets.all(0),
+          splashRadius: 16,
+        )
+      ],
+    );
+  }
+}
