5850089e764e1b15775295ec7fd0a882796c049d
Carlos Felício <felicio.contato@gmail.com>
2023-03-22 17:27:55 -0300

    :recycle: Refactoring castList to convertDynamicToListMap

--- lib/src/shared/utils/map_tricks.dart
+++ lib/src/shared/utils/map_tricks.dart
@@ -1,7 +1,32 @@
 class MapTricks {
   const MapTricks._();
 
-  static List<Map<String, dynamic>> castList(List<dynamic> list) {
-    return list.map((item) => item as Map<String, dynamic>).toList();
+  static List<Map<String, dynamic>> convertDynamicToListMap(dynamic value) {
+    if (value is Map<String, dynamic>) {
+      // verifica se o valor já é um Map<String, dynamic>
+      return [value];
+    } else if (value is List) {
+      // verifica se o valor é uma lista
+      List<Map<String, dynamic>> result = [];
+      for (var item in value) {
+        // itera sobre cada item da lista
+        if (item is Map<String, dynamic>) {
+          // se o item já for um Map<String, dynamic>, adiciona na lista result
+          result.add(item);
+        } else if (item is List) {
+          // se o item for uma lista, chama a função recursivamente para converter cada item da lista em um Map<String, dynamic>
+          List<Map<String, dynamic>> nestedResult =
+              convertDynamicToListMap(item);
+          result.addAll(nestedResult);
+        } else {
+          // se o item não for nem um Map<String, dynamic> nem uma lista, retorna a lista vazia
+          return [];
+        }
+      }
+      return result;
+    } else {
+      // se o valor não for nem um Map<String, dynamic> nem uma lista, retorna a lista vazia
+      return [];
+    }
   }
 }
